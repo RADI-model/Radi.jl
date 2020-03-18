b2e03d6a3c68a925c7d7937baa0eae676bc5d2d2
mvdh7 <m.p.humphreys@icloud.com>
2020-03-18 14:35:26 +0100

    Establish very basic workflow, model time loop and variable saving

--- RADI.jl
+++ RADI.jl
@@ -1,6 +1,60 @@
 module RADI
-export say_RADI
 
-say_RADI() = println("RADI!")
+"""
+    timeprep(t_start, t_end, t_interval, saveperXsteps)
+
+Prepare vectors of model timesteps and savepoints. All time units are in days.
+"""
+function timeprep(t_start, t_end, t_interval, saveperXsteps)
+    timesteps = t_start:t_interval:t_end
+    ntps = length(timesteps)
+    savepoints = collect(1:saveperXsteps:ntps)
+    # Save final timepoint if it's not already in the list
+    if !(ntps in savepoints)
+        append!(savepoints, ntps)
+    end
+    nsps = length(savepoints)
+    return timesteps, savepoints, ntps, nsps
+end # function timeprep
+
+"""
+    model()
+
+Run the RADI model.
+"""
+function model(t_start, t_end, t_interval, saveperXsteps)
+
+    timesteps, savepoints, ntps, nsps = timeprep(t_start, t_end, t_interval, saveperXsteps)
+    sp = 1
+
+    # Set water column conditions
+    oxy_w = 400.0 # micromol/kg
+
+    # Create arrays for modelled variables
+    depths = LinRange(0.0, 20.0, 21) # in cm
+    ndepths = length(depths)
+    oxy = fill(oxy_w, (ndepths,))
+
+    # Create arrays for saved variables
+    oxy_save = fill(NaN, (ndepths, nsps))
+
+    # Run RADI run: main model loop
+    for (i, t) in enumerate(timesteps)
+
+        # Model changes in variables
+        oxy += depths .+ 0.1
+
+        # Save output if we are at a savepoint
+        if i in savepoints
+            oxy_save[:, sp] = oxy
+            sp += 1
+        end # if
+    end # for t
+
+    return depths, oxy_save
+
+end # function model
+
+say_RADI() = println("RADI done!")
 
 end # module RADI
--- testing.jl
+++ testing.jl
@@ -1,7 +1,15 @@
 module Tst
 
+using Plots
+Plots.default(show=true)
+Plots.closeall()
+
 include("RADI.jl")
 import .RADI
+
+depths, oxy = RADI.model(0.0, 10.0, 0.1, 10)
+
+plot(depths, oxy)
 
 RADI.say_RADI()
 
