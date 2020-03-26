10b6ce758ff6d8dd18a434ceaefd095d50cfc7c7
mvdh7 <m.p.humphreys@icloud.com>
2020-03-26 13:50:04 +0100

    Update diffusion methods

--- RADI.jl
+++ RADI.jl
@@ -51,12 +51,14 @@ function model(stoptime, interval, saveperXsteps)
     z_res = 0.001
     z_res2 = z_res^2
     z_max = 0.2
-    depths = 0:z_res:z_max # in m
+    depths = collect(-z_res:z_res:(z_max+z_res)) # in m
+    depths[1] = NaN
+    depths[end] = NaN
     ndepths = length(depths)
-    oxy0 = fill(oxy_w, (ndepths,))
-    oxy = copy(oxy0)
-    poc0 = fill(10.0, (ndepths,))
-    poc = copy(poc0)
+    oxy0 = fill(oxy_w*0.5, ndepths)
+    oxy = copy(oxy0)*0
+    poc0 = fill(10.0, ndepths)
+    poc = copy(poc0)*0
 
     # Depth-dependent porosity
     phi0 = 0.85
@@ -74,10 +76,10 @@ function model(stoptime, interval, saveperXsteps)
     Foc = 1.0 # flux of total organic carbon to the bottom [mol/m2/a]
     Ftot = Foc.*M_OM # total sediment flux [g/m2/a]
     rho_pom = 2.65e6 # solid density [g/m3]
-    v0 = Ftot/(rho_pom*phiS[1]) # [m/a] bulk burial velocity at sediment-water interface
-    vinf = v0*phiS[1]/phiS[end] # [m/a] bulk burial velocity at the infinite depth
-    u = vinf*phi[end]./phi # [m/a] porewater burial velocity
-    w = vinf*phiS[end]./phiS # [m/a] solid burial velocity
+    v0 = Ftot/(rho_pom*phiS[2]) # [m/a] bulk burial velocity at sediment-water interface
+    vinf = v0*phiS[2]/phiS[end-1] # [m/a] bulk burial velocity at the infinite depth
+    u = vinf*phi[end-1]./phi # [m/a] porewater burial velocity
+    w = vinf*phiS[end-1]./phiS # [m/a] solid burial velocity
 
     # Diffusive boundary layer
     dbl = 1e-3 # thickness at location taken from Sulpis et al. 2018 PNAS [m]
@@ -87,15 +89,15 @@ function model(stoptime, interval, saveperXsteps)
     D_oxy_tort2 = D_oxy./tort2
 
     # Bioturbation (for solids)
-    D_bio_0 = @. 0.0232e-4*(Foc*1e2)^0.85 # [m2/a] surf bioturb coeff, Archer et al (2002)
+    D_bio_0 = @. 0.0232e-4*(1e2Foc)^0.85 # [m2/a] surf bioturb coeff, Archer et al (2002)
     lambda_b = 0.08 # [m] characteristic depth of Archer et al. (2002), value
                     # here following Martin & Sayles (1990)
     D_bio = @. D_bio_0*exp(-(depths/lambda_b)^2)*oxy_w/(oxy_w + 0.02)
     # ^[m2/a] bioturb coeff, Archer et al (2002)
 
     # Irrigation (for solutes)
-    alpha_0 = @. 11.0*(atan((5.0*Foc*1e2 - 400.0)/400.0)/pi + 0.5) - 0.9 +
-        20.0*(oxy_w/(oxy_w + 0.01))*exp(-oxy_w/0.01)*Foc*1e2/(Foc*1e2 + 30.0)
+    alpha_0 = @. 11.0*(atan((1e2Foc*5.0 - 400.0)/400.0)/pi + 0.5) - 0.9 +
+        20.0*(oxy_w/(oxy_w + 0.01))*exp(-oxy_w/0.01)*1e2Foc/(1e2Foc + 30.0)
     # ^[/a] from Archer et al (2002)
     lambda_i = 0.05 # [m] mysterious characteristic depth
     alpha = @. alpha_0*exp(-(depths/lambda_i)^2)
@@ -122,29 +124,8 @@ function model(stoptime, interval, saveperXsteps)
     TR = @. 2z_res*tort2/dbl
 
     # Create arrays for saved variables
-    oxy_save = fill(NaN, (ndepths, nsps))
-    poc_save = fill(NaN, (ndepths, nsps))
-
-    "Diffusion within the sediments."
-    function diffuse!(z::Int, var0::Array{Float64,1}, var::Array{Float64,1},
-            D_var::Float64)
-        # In MATLAB, there was an extra factor of 2 in here?
-        var[z] += interval*(var0[z+1] - 2.0var0[z] + var0[z-1])*D_var/z_res2
-    end # function diffuse!
-
-    "Diffusion across the sediment-water interface."
-    function diffuse!(var0::Array{Float64,1}, var::Array{Float64,1},
-            D_var::Float64, var_w::Float64)
-        var[1] += interval*(2.0(var0[2] - var0[1]) + TR[1]*(var_w - var0[1]))*
-            D_var/z_res2
-    end # function diffuse!
-
-    "Diffusion at the bottom of the modelled sediment."
-    function diffuse!(var0::Array{Float64,1}, var::Array{Float64,1},
-            D_var::Float64)
-        var[ndepths] += interval*2.0(var0[ndepths-1] - var0[ndepths])*
-            D_var/z_res2
-    end # function diffuse!
+    oxy_save = fill(NaN, (ndepths-2, nsps))
+    poc_save = fill(NaN, (ndepths-2, nsps))
 
     "Irrigation throughout the sediment."
     function irrigate!(z::Int, var0::Array{Float64,1}, var::Array{Float64,1},
@@ -189,61 +170,100 @@ function model(stoptime, interval, saveperXsteps)
             (var0[ndepths] - var0[ndepths-1])/z_res
     end #function accumulate!
 
+    "Substitute in above-surface value for solutes."
+    function surface!(var0::Array{Float64,1}, var_w::Float64)
+        # Equation following Boudreau (1996, method-of-lines):
+        n = 2 # ambiguous value from Eq. (104)
+        var0[1] = var0[3] + (var_w - var0[2])*2z_res/(dbl*phi[2]^(n+1))
+        # # Or, equation following RADI.m v20:
+        # var0[1] = var0[3] + (var_w - var0[2])*2z_res*tort2[2]/dbl
+    end # function surface!
+
+    "Substitute in above-surface value for solids."
+    function surface!(var0::Array{Float64,1}, Fvar::Float64, D_var_0::Float64)
+        var0[1] = var0[3] + (Fvar/phiS[2] - w[2]*var0[2])*2z_res/D_var_0
+    end # function surface!
+
+    "Substitute in below-bottom value."
+    function bottom!(var0::Array{Float64,1})
+        var0[end] = var0[end-2]
+    end # function bottom!
+
+    "Diffusion within the sediments."
+    function diffuse!(z::Int, var0::Array{Float64,1}, var::Array{Float64,1},
+            D_var::Float64)
+        # In MATLAB, there was an extra factor of 2 in here?
+        var[z] += interval*(var0[z-1] - 2.0var0[z] + var0[z+1])*D_var/z_res2
+    end # function diffuse!
+
     # ===== Run RADI run: main model loop ======================================
     for t in 1:ntps
-        tsave = t in savepoints # do we save this time?
-        for z in 1:ndepths
+        tsave = t in savepoints # i.e. do we save this time?
+
+        # Substitutions above the sediment-water interface
+        surface!(oxy0, oxy_w)
+        surface!(poc0, Foc, D_bio_0)
+
+        # Substitutions below the bottom of the sediment
+        bottom!(oxy0)
+        bottom!(poc0)
+
+        for z in 2:(ndepths-1)
         # ~~~ BEGIN SEDIMENT PROCESSING ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        # --- At the sediment-water interface (the top of the sediment): -------
-            if z == 1
-                diffuse!(oxy0, oxy, D_oxy_tort2[z], oxy_w)
-                diffuse!(poc0, poc, D_bio[z], poc0[z])
-                bury!(oxy0, oxy, oxy_w)
-                accumulate!(poc0, poc, )
-        # --- At the bottom of the modelled sediment: --------------------------
-            elseif z == ndepths
-                diffuse!(oxy0, oxy, D_oxy_tort2[z])
-                diffuse!(poc0, poc, D_bio[z])
-                accumulate!(poc0, poc)
-        # --- Only internally within the sediment (excl. top and bottom): ------
-            else
-                diffuse!(z, oxy0, oxy, D_oxy_tort2[z])
-                diffuse!(z, poc0, poc, D_bio[z])
-                bury!(z, oxy0, oxy, D_oxy)
-                accumulate!(z, poc0, poc)
-            end # if
-        # --- Everywhere within the sediment (incl. top and bottom): -----------
-            irrigate!(z, oxy0, oxy, oxy_w)
-            Rg_z = poc[z]*krefractory[z]
-            react!(z, oxy, -Rg_z*phiS_phi[z])
-            react!(z, poc, -Rg_z)
+
+            diffuse!(z, oxy0, oxy, D_oxy_tort2[z])
+            diffuse!(z, poc0, poc, D_bio[z])
+
+        # # --- At the sediment-water interface (the top of the sediment): -------
+        #     if z == 2
+        #         diffuse!(oxy0, oxy, D_oxy_tort2[z], oxy_w)
+        #         diffuse!(poc0, poc, D_bio[z], poc0[z])
+        #         # bury!(oxy0, oxy, oxy_w)
+        #         # accumulate!(poc0, poc, )
+        # # --- At the bottom of the modelled sediment: --------------------------
+        #     elseif z == ndepths-1
+        #         diffuse!(oxy0, oxy, D_oxy_tort2[z])
+        #         diffuse!(poc0, poc, D_bio[z])
+        #         # accumulate!(poc0, poc)
+        # # --- Only internally within the sediment (excl. top and bottom): ------
+        #     else
+        #         diffuse!(z, oxy0, oxy, D_oxy_tort2[z])
+        #         diffuse!(z, poc0, poc, D_bio[z])
+        #         # bury!(z, oxy0, oxy, D_oxy)
+        #         # accumulate!(z, poc0, poc)
+        #     end # if
+        # # --- Everywhere within the sediment (incl. top and bottom): -----------
+        #     # irrigate!(z, oxy0, oxy, oxy_w)
+        #     # Rg_z = poc[z]*krefractory[z]
+        #     # react!(z, oxy, -Rg_z*phiS_phi[z])
+        #     # react!(z, poc, -Rg_z)
         # ~~~ END SEDIMENT PROCESSING ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
             # Save output if we are at a savepoint
             if tsave
                 if t == 1
-                    oxy_save[z, sp] = oxy0[z]
-                    poc_save[z, sp] = poc0[z]
+                    oxy_save[z-1, sp] = oxy0[z]
+                    poc_save[z-1, sp] = poc0[z]
                 else
-                    oxy_save[z, sp] = oxy[z]
-                    poc_save[z, sp] = poc[z]
+                    oxy_save[z-1, sp] = oxy[z]
+                    poc_save[z-1, sp] = poc[z]
                 end
-                if z == ndepths
+                if z == ndepths-1
                     println("RADI: reached savepoint $sp (step $t of $ntps)...")
                     sp += 1
                 end # if
             end # if
-        end # for z
+        end # for z in 2:(ndepths-1)
 
         # Copy result into "previous step" arrays
         for z in 1:ndepths
             oxy0[z] = oxy[z]
             poc0[z] = poc[z]
-        end # for z
+        end # for z in 1:ndepths
     end # for t
     # ===== End of main model loop =============================================
 
-    return depths, oxy_save, poc_save
+    return depths[2:end-1], oxy_save, poc_save
 
 end # function model
 
