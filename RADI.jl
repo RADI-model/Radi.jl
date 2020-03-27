526f2f77bc27690a0c81bb2b6af0635d379f9898
mvdh7 <m.p.humphreys@icloud.com>
2020-03-27 15:55:43 +0100

    Fully functional model and docs complete

--- RADI.jl
+++ RADI.jl
@@ -1,5 +1,7 @@
 module RADI
 
+using Base.SimdLoop
+
 include("gsw_rho.jl")
 
 """
@@ -7,15 +9,15 @@ include("gsw_rho.jl")
 
 Prepare vectors of model timesteps and savepoints. All time units are in days.
 """
-function timeprep(stoptime, interval, saveperXsteps)
-    timesteps = 0.0:interval:stoptime
-    ntps = length(timesteps)
-    savepoints = collect(1:saveperXsteps:ntps)
+function timeprep(stoptime::Float64, interval::Float64, saveperXsteps::Int)
+    timesteps::Array{Float64,1} = collect(0.0:interval:stoptime)
+    ntps::Int = length(timesteps)
+    savepoints::Array{Float64,1} = collect(1:saveperXsteps:ntps)
     # Save final timepoint if it's not already in the list
     if !(ntps in savepoints)
         append!(savepoints, ntps)
     end
-    nsps = length(savepoints)
+    nsps::Int = length(savepoints)
     return timesteps, savepoints, ntps, nsps
 end # function timeprep
 
@@ -24,128 +26,153 @@ end # function timeprep
 
 Run the RADI model.
 """
-function model(stoptime, interval, saveperXsteps)
+function model(stoptime::Float64, interval::Float64, saveperXsteps::Int,
+    oxy_i, poc_i)
 # ==============================================================================
 # === User inputs/settings =====================================================
 # ==============================================================================
-# Overlying water conditions
-T = 1.4 # temperature / degC
-S = 34.69 # practical salinity
-rho_sw = gsw_rho(S, T, 1) # seawater density [kg/m^3]
-oxy_w = 159.7e-6*rho_sw # dissolved oxygen [mol/m3]
-po4_w = 2.39e-6*rho_sw # phosphate from GLODAP at station location, bottom
-                       # waters [mol/m3]
+
+# Model time grid: function inputs
 # Model depth grid
-z_res = 0.001
-z_max = 0.2
+z_res::Float64 = 0.05e-2 # m
+z_max::Float64 = 20e-2 # m
+
+# Overlying water conditions
+T::Float64 = 1.4 # temperature / degC
+S::Float64 = 34.69 # practical salinity
+rho_sw::Float64 = gsw_rho(S, T, 1) # seawater density [kg/m^3]
+oxy_w::Float64 = 159.7e-6*rho_sw # dissolved oxygen [mol/m3]
+po4_w::Float64 = 2.39e-6*rho_sw # phosphate from GLODAP at station location,
+                                # bottom waters [mol/m3]
+dbl::Float64 = 1e-3 # thickness at location taken from Sulpis et al. 2018 PNAS [m]
+
+# Organic matter flux to the surface sediment
+Ftot::Float64 = 36.45 # flux of particulate organic matter to seafloor [g/m2/a]
+# Foc = 1.0 # flux of total organic carbon to the bottom [mol/m2/a]
+rho_pom::Float64 = 2.65e6 # solid density [g/m3]
+
 # Sediment porosity
 # Porosity profile (porewater bulk fraction) fitted from station7 mooring3
 # of cruise NBP98-2 by Sayles et al. DSR 2001
-phi0 = 0.85
-phiInf = 0.74
-beta = 33.0
-# Organic matter flux to the surface sediment
-Foc = 1.0 # flux of total organic carbon to the bottom [mol/m2/a]
-rho_pom = 2.65e6 # solid density [g/m3]
-poc_initial = 100.0 # initial POC in the sediment
-# Diffusive boundary layer
-dbl = 1e-3 # thickness at location taken from Sulpis et al. 2018 PNAS [m]
+phiInf::Float64 = 0.74
+phi0::Float64 = 0.85
+beta::Float64 = 33.0
+
 # Characteristic depths
-lambda_b = 0.08 # [m] characteristic depth of Archer et al. (2002), value
-                # here following Martin & Sayles (1990)
-lambda_i = 0.05 # [m] characteristic depth for irrigation
+lambda_b::Float64 = 0.08 # [m] characteristic depth of Archer et al. (2002),
+                         # value here following Martin & Sayles (1990)
+lambda_i::Float64 = 0.05 # [m] characteristic depth for irrigation
 
 # ==============================================================================
 # === The model: user changes nothing below here ===============================
 # ==============================================================================
 
-# Set up model time variables
-timesteps, savepoints, ntps, nsps = timeprep(stoptime, interval, saveperXsteps)
-sp = 1
+# Set up model time grid
+timesteps::Array{Float64,1}, savepoints::Array{Float64,1}, ntps::Int,
+    nsps::Int = timeprep(stoptime, interval, saveperXsteps)
+sp::Int = 1
 
-# "Redfield" ratios
-RC = @. 1/(6.9e-3po4_w/(1e-6rho_sw) + 6e-3)
-# ^P:C computed as a function of SRP from Galbraith and Martiny PNAS 2015
-RN = 11 # value at 60 degS from Martiny et al. Nat G 2013
-RP = 1 # Redfield ratio for P in the deep sea
-M_OM = @. 30.03 + (RN/RC)*17.03 + (RP/RC)*97.994
-# ^ Organic Matter molar mass [g of OM per mol of OC]
-
-# Create arrays for modelled variables
-z_res2 = z_res^2
-depths = collect(-z_res:z_res:(z_max+z_res)) # in m
+# Set up depth grid
+z_res2::Float64 = z_res^2
+depths::Array{Float64,1} = collect(-z_res:z_res:(z_max+z_res)) # in m
 depths[1] = NaN
 depths[end] = NaN
-ndepths = length(depths)
-oxy0 = fill(oxy_w, ndepths)
-oxy = copy(oxy0)
-poc0 = fill(poc_initial, ndepths)
-poc = copy(poc0)
+ndepths::Int = length(depths)
+
+"Create arrays for modelled variables with a constant start value."
+function makearrays(var_start::Float64)
+    var0::Array{Float64,1} = fill(var_start, ndepths)
+    var::Array{Float64,1} = copy(var0)
+    var_save::Array{Float64,2} = fill(NaN, (ndepths-2, nsps))
+    return var0, var, var_save
+end # function makearrays
+
+"Create arrays for modelled variables with starting array provided."
+function makearrays(var_start::Array{Float64,1})
+    var0::Array{Float64,1} = vcat(NaN, var_start, NaN)
+    var::Array{Float64,1} = copy(var0)
+    var_save::Array{Float64,2} = fill(NaN, (ndepths-2, nsps))
+    return var0, var, var_save
+end # function makearrays
+
+# Create arrays for modelled variables
+oxy0, oxy, oxy_save = makearrays(oxy_i)
+poc0, poc, poc_save = makearrays(poc_i)
+
+# "Redfield" ratios
+RC::Float64 = @. 1.0/(6.9e-3po4_w/(1e-6rho_sw) + 6e-3)
+# ^P:C computed as a function of SRP from Galbraith and Martiny PNAS 2015
+RN::Float64 = 11.0 # value at 60 degS from Martiny et al. Nat G 2013
+RP::Float64 = 1.0 # Redfield ratio for P in the deep sea
+M_CH2O::Float64 = 30.031 # g/mol
+M_NH3::Float64 = 17.031 # g/mol
+M_H3PO4::Float64 = 97.994 # g/mol
+# M_OM = @. M_CH2O + (RN/RC)*M_NH3 + (RP/RC)*M_H3PO4
+# # ^ Organic Matter molar mass [g of OM per mol of OC]
+M_POM::Float64 = RC*M_CH2O + RN*M_NH3 + RP*M_H3PO4 # molar mass of POM in g/mol
+Ftot_mol::Float64 = Ftot/M_POM
+Foc::Float64 = Ftot_mol*RC
 
 # Depth-dependent porosity
-phi = @. (phi0 - phiInf)*exp(-beta*depths) + phiInf
-phiS = 1.0 .- phi # solid volume fraction
-phiS_phi = phiS./phi
-tort2 = @. 1.0 - 2.0log(phi) # tortuosity squared from Boudreau (1996, GCA)
-tort = sqrt.(tort2) # tortuosity
-
-# Solid fluxes and solid initial conditions
-Ftot = Foc.*M_OM # total sediment flux [g/m2/a]
-v0 = Ftot/(rho_pom*phiS[2]) # [m/a] bulk burial velocity at sediment-water
-                            # interface
-vinf = v0*phiS[2]/phiS[end-1] # [m/a] bulk burial velocity at the infinite depth
-u = vinf*phi[end-1]./phi # [m/a] porewater burial velocity
-w = vinf*phiS[end-1]./phiS # [m/a] solid burial velocity
-
-# Temperature-dependent "free solution" diffusion coefficients
-D_oxy = @. 0.034862 + 0.001409T # [m2/a] oxygen diffusion coefficient from Li
-                                # and Gregory
-D_oxy_tort2 = D_oxy./tort2
+phi::Array{Float64,1} = @. (phi0 - phiInf)*exp(-beta*depths) + phiInf
+phiS::Array{Float64,1} = 1.0 .- phi # solid volume fraction
+phiS_phi::Array{Float64,1} = phiS./phi
+tort2::Array{Float64,1} = @. 1.0 - 2.0log(phi)
+# ^tortuosity squared from Boudreau (1996, GCA)
+# tort::Array{Float64,1} = sqrt.(tort2) # tortuosity
+delta_phi::Array{Float64,1} = @. -beta*(phi0 - phiInf)*exp(-beta*depths)
+delta_phiS::Array{Float64,1} = -delta_phi
+delta_tort2i::Array{Float64,1} = @. 2.0delta_phi/(phi*tort2^2)
+delta_tort2_tort2::Array{Float64,1} = delta_tort2i.*tort2
 
 # Bioturbation (for solids)
-D_bio_0 = @. 0.0232e-4*(1e2Foc)^0.85 # [m2/a] surf bioturb coeff, Archer et al.
-                                     # (2002)
-D_bio = @. D_bio_0*exp(-(depths/lambda_b)^2)*oxy_w/(oxy_w + 0.02)
-# ^[m2/a] bioturb coeff, Archer et al (2002)
-
-# Irrigation (for solutes)
-alpha_0 = @. 11.0*(atan((1e2Foc*5.0 - 400.0)/400.0)/pi + 0.5) - 0.9 +
-    20.0*(oxy_w/(oxy_w + 0.01))*exp(-oxy_w/0.01)*1e2Foc/(1e2Foc + 30.0)
-# ^[/a] from Archer et al (2002)
-alpha = @. alpha_0*exp(-(depths/lambda_i)^2)
-# ^[/a] Archer et al (2002) the depth of 5 cm was changed
-
-# Depth-dependent porosity and diffusion coefficient loss
-delta_phi = @. -beta*(phi0 - phiInf)*exp(-beta*depths)
-delta_phiS = @. beta*(phi0 - phiInf)*exp(-beta*depths)
-delta_tort2 = @. 2.0beta*(phi0 - phiInf)/
-    (phi0 + phiInf*(exp(beta*depths) - 1.0))
-delta_D_bio = @. -(2.0D_bio_0*depths/lambda_b^2)*exp(-(depths/lambda_b)^2)*
+D_bio_0::Float64 = @. 0.0232e-4*(1e2Foc)^0.85
+# ^[m2/a] surf bioturb coeff, Archer et al. (2002)
+D_bio::Array{Float64,1} = @. D_bio_0*exp(-(depths/lambda_b)^2)*
     oxy_w/(oxy_w + 0.02)
-# delta_phi = append!([0.0], diff(phi)) # depth-dependent porosity loss
-# delta_phiS = append!([0.0], diff(phiS)) # depth-dependent solid fraction gain
-# delta_tort2 = append!([0.0], diff(tort.^2)) # depth-dependent tortuosity gain
-# delta_D_bio = append!([0.0], diff(D_bio)) # [m/a]
+# ^[m2/a] bioturb coeff, Archer et al (2002)
+delta_D_bio::Array{Float64,1} = @. -2.0depths*D_bio/lambda_b^2
+
+# Organic matter degradation parameters
+krefractory::Array{Float64,1} = @. 80.25D_bio_0*exp(-depths)
+# ^[/a] from Archer et al (2002)
+
+# Solid fluxes and solid initial conditions
+# Ftot = Foc.*M_OM # total sediment flux [g/m2/a]
+x0::Float64 = Ftot/(rho_pom*phiS[2])
+# ^[m/a] bulk burial velocity at sediment-water interface
+xinf::Float64 = x0*phiS[2]/phiS[end-1]
+# ^[m/a] bulk burial velocity at the infinite depth
+u::Array{Float64,1} = xinf*phi[end-1]./phi # [m/a] porewater burial velocity
+w::Array{Float64,1} = xinf*phiS[end-1]./phiS # [m/a] solid burial velocity
 
 # Biodiffusion depth-attenuation: see Boudreau (1996); Fiadeiro and Veronis
 # (1977)
-Peh = @. w*z_res/2.0D_bio # one half the cell Peclet number (Eq. 97 in Boudreau
-                          # 1996)
+Peh::Array{Float64,1} = @. w*z_res/2.0D_bio
+# ^one half the cell Peclet number (Eq. 97 in Boudreau 1996)
 # when Peh<<1, biodiffusion dominates, when Peh>>1, advection dominates
-sigma = @. 1.0/tanh(Peh) - 1.0/(Peh) # Eq. 96 in Boudreau 1996
+sigma::Array{Float64,1} = @. 1.0/tanh(Peh) - 1.0/(Peh) # Eq. 96 in Boudreau 1996
+sigma1m::Array{Float64,1} = 1.0 .- sigma
+sigma1p::Array{Float64,1} = 1.0 .+ sigma
 
-# Organic matter degradation parameters
-krefractory = @. 80.25D_bio_0*exp(-depths) # [/a] from Archer et al (2002)
+# vvv NOT YET IN THE PARAMETERS PART OF THE DOCUMENTATION vvvvvvvvvvvvvvvvvvvvvv
+# Temperature-dependent "free solution" diffusion coefficients
+D_oxy::Float64 = @. 0.034862 + 0.001409T
+# ^[m2/a] oxygen diffusion coefficient from Li and Gregory
+D_oxy_tort2::Array{Float64,1} = D_oxy./tort2
 
-# Short-cut transport variables
-APPW = @. w - delta_D_bio - delta_phiS*D_bio/phiS
-DFF = @. (tort2*delta_phi/phi - delta_tort2)/tort2^2
-# DBF = @. D_bio*(phiS - delta_phi) # not used?
-# TR = @. 2z_res*tort2/dbl
+# Irrigation (for solutes)
+alpha_0::Float64 = @. 11.0*(atan((1e2Foc*5.0 - 400.0)/400.0)/pi + 0.5) - 0.9 +
+    20.0*(oxy_w/(oxy_w + 0.01))*exp(-oxy_w/0.01)*1e2Foc/(1e2Foc + 30.0)
+# ^[/a] from Archer et al (2002)
+alpha::Array{Float64,1} = @. alpha_0*exp(-(depths/lambda_i)^2)
+# ^[/a] Archer et al (2002) the depth of 5 cm was changed
 
-# Create arrays for saved variables
-oxy_save = fill(NaN, (ndepths-2, nsps))
-poc_save = fill(NaN, (ndepths-2, nsps))
+APPW::Array{Float64,1} = @. w - delta_D_bio - delta_phiS*D_bio/phiS
+TR::Float64 = 2.0z_res*tort2[2]/dbl
+Foc_phiS_0::Float64 = Foc/phiS[2]
+zr_Db_0::Float64 = 2.0z_res/D_bio_0
+# ^^^ NOT YET IN THE PARAMETERS PART OF THE DOCUMENTATION ^^^^^^^^^^^^^^^^^^^^^^
 
 "Substitute in above-surface value for solutes."
 function surface!(var0::Array{Float64,1}, var_w::Float64)
@@ -153,12 +180,12 @@ function surface!(var0::Array{Float64,1}, var_w::Float64)
     # n = 2 # ambiguous value from Eq. (104)
     # var0[1] = var0[3] + (var_w - var0[2])*2z_res/(dbl*phi[2]^(n+1))
     # Or, equation following RADI-Matlab and CANDI-Fortran:
-    var0[1] = var0[3] + (var_w - var0[2])*2.0z_res*tort2[2]/dbl
+    var0[1] = var0[3] + (var_w - var0[2])*TR
 end # function surface!
 
 "Substitute in above-surface value for solids."
-function surface!(var0::Array{Float64,1}, Fvar::Float64, D_var_0::Float64)
-    var0[1] = var0[3] + (Fvar/phiS[2] - w[2]*var0[2])*2.0z_res/D_var_0
+function surface!(var0::Array{Float64,1})
+    var0[1] = var0[3] + (Foc_phiS_0 - w[2]*var0[2])*zr_Db_0
 end # function surface!
 
 "Substitute in below-bottom value."
@@ -173,25 +200,35 @@ function substitute!(var0::Array{Float64,1}, var_w::Float64)
 end # function substitute!
 
 "Substitute in above-surface and below-bottom values for solids."
-function substitute!(var0::Array{Float64,1}, Fvar::Float64,
-        D_var_0::Float64)
-    surface!(var0, Fvar, D_var_0)
+function substitute!(var0::Array{Float64,1})
+    surface!(var0)
     bottom!(var0)
 end # function substitute!
 
-"Reactions throughout the sediment, solutes and solids."
-function react(rate::Float64)
+"Calculate reaction for solutes and solids."
+function react(rate::Float64)::Float64
     return interval*rate
-end # function react!
+end # function react
 
-"Reactions throughout the sediment, solutes and solids."
+"Apply reaction for solutes and solids."
 function react!(z::Int, var::Array{Float64,1}, rate::Float64)
     var[z] += react(rate)
 end # function react!
 
+"Advection for solutes."
+function advect!(z::Int, var0::Array{Float64,1}, var::Array{Float64,1},
+        D_var::Float64)
+    var[z] += -interval*(u[z] - D_var/phi[z] - D_var*delta_tort2_tort2[z])*
+        (var0[z+1] - var0[z-1])/(2.0z_res)
+end # function advect!
 
+"Advection for solids."
+function advect!(z::Int, var0::Array{Float64,1}, var::Array{Float64,1})
+    var[z] += -interval*APPW[z]*(sigma1m[z]*var0[z+1] + 2.0sigma[z]*var0[z] -
+        sigma1p[z]*var0[z-1])/(2.0z_res)
+end # function advect!
 
-"Diffusion throughout the sediment, solutes and solidst."
+"Diffusion throughout the sediment, solutes and solids."
 function diffuse!(z::Int, var0::Array{Float64,1}, var::Array{Float64,1},
         D_var::Float64)
     var[z] += interval*(var0[z-1] - 2.0var0[z] + var0[z+1])*D_var/z_res2
@@ -205,24 +242,26 @@ end # function irrigate!
 
 # ===== Run RADI run: main model loop ==========================================
 for t in 1:ntps
-    tsave = t in savepoints # i.e. do we save this time?
+    tsave::Bool = t in savepoints # i.e. do we save this time?
 
     # Substitutions above and below the modelled sediment column
     substitute!(oxy0, oxy_w)
-    substitute!(poc0, Foc, D_bio_0)
+    substitute!(poc0)
 
     for z in 2:(ndepths-1)
     # ~~~ BEGIN SEDIMENT PROCESSING ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         # First, do all the physical processes
+        advect!(z, oxy0, oxy, D_oxy_tort2[z])
         diffuse!(z, oxy0, oxy, D_oxy_tort2[z])
         irrigate!(z, oxy0, oxy, oxy_w)
 
+        advect!(z, poc0, poc)
         diffuse!(z, poc0, poc, D_bio[z])
 
         # Calculate maximum reaction rates based on previous timestep
-        R_poc = -poc0[z]*krefractory[z]
-        R_oxy = R_poc*phiS_phi[z]
+        R_poc::Float64 = -poc0[z]*krefractory[z]
+        R_oxy::Float64 = R_poc*phiS_phi[z]
 
         # Check maximum reaction rates are possible after other processes
         # have acted in this timestep, and correct them if not
--- testing.jl
+++ testing.jl
@@ -1,25 +1,71 @@
 module tst
 
-using Colors, Plots
+using Colors, Plots, Profile, ProfileView
 Plots.default(show=true)
 Plots.closeall()
 
 include("RADI.jl")
 import .RADI
 
-depths, oxy, poc = RADI.model(10.0, 1/128000, 128000)
-cmap = colormap("RdBu", size(oxy)[2])
+stoptime = 5.0
+interval = 0.5/128000
+saveperXsteps = 2*128000
+oxy_i = 0.0
+poc_i = 0.0
 
-cs = 1
-p1 = plot(depths*100, oxy[:, 1]*1e3, legend=false, c=cmap[cs])
-p2 = plot(depths*100, poc[:, 1], legend=false, c=cmap[cs])
-for sp in 2:size(oxy)[2]
-    plot!(p1, depths*100, oxy[:, sp]*1e3, legend=false, c=cmap[cs+sp-1])
-    plot!(p2, depths*100, poc[:, sp], legend=false, c=cmap[cs+sp-1])
-end # for sp
-plot(p1, p2, layout=(2, 1))
+function radiplot(oxy_i, poc_i)
+    @time depths, oxy, poc = RADI.model(stoptime, interval, saveperXsteps,
+        oxy_i, poc_i)
+    ntps = size(oxy)[2]
+    cmap = colormap("RdBu", ntps)
+    cs = ntps
+    p1 = plot(depths*100, oxy[:, 1]*1e3, legend=false, c=cmap[cs])
+    p2 = plot(depths*100, poc[:, 1], legend=false, c=cmap[cs])
+    for sp in 2:ntps
+        plot!(p1, depths*100, oxy[:, sp]*1e3, legend=false, c=cmap[cs-sp+1])
+        plot!(p2, depths*100, poc[:, sp], legend=false, c=cmap[cs-sp+1])
+    end # for sp
+    plot(p1, p2, layout=(2, 1))
+    return depths, oxy, poc
+end # function radiplot
 
-RADI.say_RADI()
+showprofile = false
+if showprofile
+    Profile.clear()
+    @profile depths, oxy, poc = RADI.model(stoptime, interval, saveperXsteps,
+        oxy_i, poc_i)
+    ProfileView.view()
+    RADI.say_RADI()
+else
+    depths, oxy, poc = radiplot(oxy_i, poc_i);
+end
+
+# using Base.SimdLoop
+#
+# function forsimd(xmax::Int)
+#     y = 0
+#     @simd for x in 1:xmax
+#         y += sqrt(x)
+#     end # for x
+#     return y
+# end
+#
+# function fornormal(xmax::Int)
+#     y = 0
+#     for x in 1:xmax
+#         y += sqrt(x)
+#     end # for x
+#     return y
+# end
+#
+# xmax = 10000000
+# @time forsimd(xmax)
+# @time fornormal(xmax)
+
+# abstract type Solid end
+#
+# getx(value) = value::Solid
+
 
 # # Trying out composite type but slower and too clever for its own good
 # struct PorewaterVariable
